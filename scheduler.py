3258b6a6d4d0b08fa92865f71fe04331fb037496
ErichJohann <97558441+ErichJohann@users.noreply.github.com>
2025-03-29 16:17:53 -0300

    Passing event as a parameter and handling errors

--- scheduler.py
+++ scheduler.py
@@ -1,5 +1,8 @@
 import os
+import sys
 import pickle
+import csv
+from datetime import datetime
 from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
@@ -9,8 +12,11 @@ SCOPES = ["https://www.googleapis.com/auth/calendar"]
 TOKEN_FILE = "token.pickle"
 CLIENT_SECRET_FILE = "client_secret.json"
 
-#Get credentials to login in google account
-def get_credentials():
+DATE_FORMAT = "%Y-%m-%dT%H:%M:%S%z" 
+# Example format: "2025-04-01T08:00:00-03:00" first of april 2025 at 8h 0min and 0 secs on -3h timezone
+
+#Get credentials to log in to google account
+def getCredentials():
     credentials = None
     
     #Token for easier authentication
@@ -33,25 +39,62 @@ def get_credentials():
 
     return credentials
 
-def main():
-    cred = get_credentials()
+#opens csv file and gets rows
+def getData(sheet):
+    with open(sheet, 'r') as sheet:
+        read = csv.DictReader(sheet)
+        #data gets a list of dictionaries
+        data = [row for row in read]
+    return data
+
+#Attempts create an event
+def setEvent(ev,ser):
+    print(f"Creating event: {ev['summary']}...")
+    ser.events().insert(calendarId="primary", body=ev).execute()
+
+
+def main(sheet):
+    cred = getCredentials()
+    #gets google calendar api service
     service = build("calendar", "v3", credentials=cred)
-    name = input("Type event name: ")
-    desc = input("Type event description: ")
+    events = getData(sheet)
 
-    event = {
-        "summary": name,
-        "description": desc,
-        "start": {
-            "dateTime": "2025-04-01T08:00:00-03:00",
-        },
-        "end": {
-            "dateTime": "2025-04-01T20:00:00-03:00",
-        }
-    }
+    #each row correspons to an event
+    for row in events:
+        #sets event to correct format
+        try:
+            #validates time format and parces to datetime
+            startTime = datetime.strptime(row["start"], DATE_FORMAT)
+            endTime = datetime.strptime(row["end"], DATE_FORMAT)
 
-    result = service.events().insert(calendarId="primary", body=event).execute()
-    print(f"Event created: {result.get('htmlLink')}")
+            event = { 
+                "summary":row["summary"],
+                "description":row["description"],
+                "location":row["location"],
+                "start": {
+                    "dateTime":startTime.isoformat()
+                },
+                "end": {
+                    "dateTime":endTime.isoformat()
+                },
+                "colorId": row["colorId"]
+            }
+            #create event
+            setEvent(event,service)
+
+        except ValueError as e:
+            print(f"invalid date time format - {e}")
+
+        except Exception as e:
+            print(f"Error creating event - {e}")
+
+
+    print('\nCalendar updated!')
+        
 
 if __name__ == "__main__":
-    main()
+    #checks presence of a spreadsheet file
+    if len(sys.argv) != 2:
+        print(f"Usage: python {sys.argv[0]} [sheet.csv]")
+    else:
+        main(sys.argv[1])
